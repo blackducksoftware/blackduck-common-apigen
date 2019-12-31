e6b7c310d5f31d00faed3be4d53ffd8f9e4db8ee
Eric Kerwin <ekerwin@synopsys.com>
2019-12-31 08:19:32 -0500

    no need for groovy and update to junit5

---
+++ src/test/java/com/synopsys/integration/create/apigen/BasicContextTest.java
@@ -0,0 +1,15 @@
+package com.synopsys.integration.create.apigen;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class BasicContextTest {
+	@Test
+    public void contextLoads() {
+	}
+
+}
