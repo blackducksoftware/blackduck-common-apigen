50e2094c9f5acc02c2c2727118d6ad856c2cbb97
crowleySynopsys <alex.crowley@synopsys.com>
2020-11-17 14:48:49 -0500

    Fix InputDataFinderTest

--- src/test/java/com/synopsys/integration/create/apigen/generation/finder/InputDataFinderTest.java
+++ src/test/java/com/synopsys/integration/create/apigen/generation/finder/InputDataFinderTest.java
@@ -1,5 +1,7 @@
 package com.synopsys.integration.create.apigen.generation.finder;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
@@ -7,9 +9,11 @@ import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import com.synopsys.integration.create.apigen.GeneratorConfig;
 import com.synopsys.integration.create.apigen.data.ClassCategories;
+import com.synopsys.integration.create.apigen.data.ClassTypeEnum;
 import com.synopsys.integration.create.apigen.data.LinkResponseDefinitions;
 import com.synopsys.integration.create.apigen.data.UtilStrings;
 import com.synopsys.integration.create.apigen.model.ClassTypeData;
@@ -25,20 +29,23 @@ public class InputDataFinderTest {
     public void getViewInputDataTest() {
         Set<String> imports = new HashSet<>();
         imports.add(UtilStrings.CORE_CLASS_PATH_PREFIX + UtilStrings.VIEW_BASE_CLASS);
-        String className = "java.util.List<ProjectViewV4>";
-        ClassTypeData classTypeData = new ClassTypeData(classCategories.computeData(className).getType(), new FilePathUtil(new GeneratorConfig()));
+        String className = "ProjectView";
+
+        FilePathUtil mockFilePathUtil = Mockito.mock(FilePathUtil.class);
+        Mockito.when(mockFilePathUtil.getOutputPathToViewFiles()).thenReturn("");
+        ClassTypeData classTypeData = new ClassTypeData(classCategories.computeData(className).getType(), mockFilePathUtil);
         Map<String, Object> inputData1 = inputDataFinder.getInputData(classTypeData, imports, className, new HashSet<>(), "json4");
 
-        assertTrue(inputData1.size() == 8);
-        assertTrue(inputData1.get(UtilStrings.PACKAGE_NAME).equals(UtilStrings.GENERATED_VIEW_PACKAGE));
-        assertTrue(inputData1.get(UtilStrings.CLASS_NAME).equals("ProjectViewV4"));
-        assertTrue(inputData1.get("imports") != null);
-        assertTrue(inputData1.get(UtilStrings.MEDIA_TYPE).equals("json4"));
+        assertEquals(8, inputData1.size());
+        assertEquals(UtilStrings.GENERATED_VIEW_PACKAGE, inputData1.get(UtilStrings.PACKAGE_NAME));
+        assertEquals("ProjectView", inputData1.get(UtilStrings.CLASS_NAME));
+        assertNotEquals(null, inputData1.get("imports"));
+        assertEquals("json4", inputData1.get(UtilStrings.MEDIA_TYPE));
 
         Set<LinkData> links = new HashSet<>();
         links.add(new LinkData("label", new ResponseDefinition("", "", "", false), new LinkResponseDefinitions()));
-        Map<String, Object> inputData2 = inputDataFinder.getInputData(null, new HashSet<>(), "", new HashSet<>(), "");
+        Map<String, Object> inputData2 = inputDataFinder.getInputData(new ClassTypeData(ClassTypeEnum.COMMON, mockFilePathUtil), new HashSet<>(), "", new HashSet<>(),  links, "");
 
-        assertTrue(inputData2.size() == 11);
+        assertEquals(11, inputData2.size());
     }
 }
