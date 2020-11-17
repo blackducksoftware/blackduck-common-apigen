370d3b52a5e49e9c440d0df1983346585f845321
Gavin Killough <gavink@synopsys.com>
2020-11-17 12:02:58 -0500

    Clean up test

--- src/test/java/com/synopsys/integration/create/apigen/generation/finder/InputDataFinderTest.java
+++ src/test/java/com/synopsys/integration/create/apigen/generation/finder/InputDataFinderTest.java
@@ -1,5 +1,7 @@
 package com.synopsys.integration.create.apigen.generation.finder;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
@@ -9,6 +11,7 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.create.apigen.GeneratorConfig;
+import com.synopsys.integration.create.apigen.GeneratorPropertiesConfig;
 import com.synopsys.integration.create.apigen.data.ClassCategories;
 import com.synopsys.integration.create.apigen.data.LinkResponseDefinitions;
 import com.synopsys.integration.create.apigen.data.UtilStrings;
@@ -17,7 +20,6 @@ import com.synopsys.integration.create.apigen.model.LinkData;
 import com.synopsys.integration.create.apigen.model.ResponseDefinition;
 
 public class InputDataFinderTest {
-
     private final InputDataFinder inputDataFinder = new InputDataFinder();
     private final ClassCategories classCategories = new ClassCategories(new ClassNameManager());
 
@@ -26,19 +28,23 @@ public class InputDataFinderTest {
         Set<String> imports = new HashSet<>();
         imports.add(UtilStrings.CORE_CLASS_PATH_PREFIX + UtilStrings.VIEW_BASE_CLASS);
         String className = "java.util.List<ProjectViewV4>";
-        ClassTypeData classTypeData = new ClassTypeData(classCategories.computeData(className).getType(), new FilePathUtil(new GeneratorConfig(null)));
+
+        GeneratorPropertiesConfig generatorPropertiesConfig = new GeneratorPropertiesConfig();
+        generatorPropertiesConfig.generatorOutputPath = "./build/tmp/test-output";
+
+        ClassTypeData classTypeData = new ClassTypeData(classCategories.computeData(className).getType(), new FilePathUtil(new GeneratorConfig(generatorPropertiesConfig)));
         Map<String, Object> inputData1 = inputDataFinder.getInputData(classTypeData, imports, className, new HashSet<>(), "json4");
 
-        assertTrue(inputData1.size() == 8);
-        assertTrue(inputData1.get(UtilStrings.PACKAGE_NAME).equals(UtilStrings.GENERATED_VIEW_PACKAGE));
-        assertTrue(inputData1.get(UtilStrings.CLASS_NAME).equals("ProjectViewV4"));
-        assertTrue(inputData1.get("imports") != null);
-        assertTrue(inputData1.get(UtilStrings.MEDIA_TYPE).equals("json4"));
+        assertEquals(8, inputData1.size());
+        assertEquals(UtilStrings.GENERATED_VIEW_PACKAGE, inputData1.get(UtilStrings.PACKAGE_NAME));
+        assertEquals("ProjectViewV4", inputData1.get(UtilStrings.CLASS_NAME));
+        assertNotNull(inputData1.get("imports"));
+        assertEquals("json4", inputData1.get(UtilStrings.MEDIA_TYPE));
 
         Set<LinkData> links = new HashSet<>();
         links.add(new LinkData("label", new ResponseDefinition("", "", "", false), new LinkResponseDefinitions()));
         Map<String, Object> inputData2 = inputDataFinder.getInputData(null, new HashSet<>(), "", new HashSet<>(), "");
 
-        assertTrue(inputData2.size() == 11);
+        assertEquals(11, inputData2.size());
     }
 }
