6fe23aaf7e15cdf6e99945dc02e042e1aa7cfaa2
crowleySynopsys <alex.crowley@synopsys.com>
2021-07-22 10:51:02 -0400

    Update headers.

--- src/main/java/com/synopsys/integration/create/apigen/data/mediatype/MediaTypePathUtility.java
+++ src/main/java/com/synopsys/integration/create/apigen/data/mediatype/MediaTypePathUtility.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-common-apigen
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.create.apigen.data.mediatype;
 
 import org.apache.commons.lang3.StringUtils;
--- src/main/java/com/synopsys/integration/create/apigen/model/JavaStrings.java
+++ src/main/java/com/synopsys/integration/create/apigen/model/JavaStrings.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-common-apigen
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.create.apigen.model;
 
 import java.util.Arrays;
